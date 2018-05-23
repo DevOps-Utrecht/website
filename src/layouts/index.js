c3cda68da8d3853a9b76e5041801751cd281c464
Emiel van Rijn <mail@emielvanrijn.nl>
2018-05-23 19:40:38 +0200

    Initial Commit! Fun++

---
+++ src/layouts/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Layout = ({ children, data }) => (
+  <div>
+    {children()}
+  </div>
+)
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IndexPage = () => (
+  <div>
+    <h1>De officiële onofficiële dev-ops website!</h1>
+  </div>
+)
+
+export default IndexPage
